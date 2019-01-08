887e0aee1156283dade69def9195d45779ed9b95
Ndelforno <nicolas.delforno@gmail.com>
2019-01-08 13:17:45 -0500

    arrays created from csv

--- report.rb
+++ report.rb
@@ -1,19 +1,45 @@
 require 'csv'
+require 'json'
 
-students = []
-marks = []
-courses = []
-tests = []
+students_array = []
+marks_array = []
+courses_array = []
+tests_array = []
 
-p students
 
-CSV.foreach('students.csv') do |row|
-  students << {:id => row[0], :name => row[1]}
+
+students = CSV.read('students.csv')[1 .. -1]
+marks = CSV.read('marks.csv')[1 .. -1]
+courses = CSV.read('courses.csv')[1 .. -1]
+tests = CSV.read('tests.csv')[1 .. -1]
+
+
+
+students.each do |student|
+  students_array << {:id => student[0], :name => student[1]}
 end
 
-CSV.foreach('marks.csv') do |row|
-  marks << {:test_id => row[0], :student_id => row[1], :mark => row[2]}
+
+marks.each do |mark|
+  marks_array << {:test_id => mark[0], :student_id => mark[1], :mark => mark[2]}
 end
 
-p students
-p marks
+courses.each do |course|
+  courses_array << {:id => course[0], :name => course[1], :teacher => course[2]}
+end
+
+tests.each do |test|
+  tests_array << {:id => test[0], :course_id => test[1], :weight => test[2]}
+end
+
+
+
+
+
+p students_array
+puts '---------'
+p marks_array
+puts '---------'
+p courses_array
+puts '---------'
+p tests_array
